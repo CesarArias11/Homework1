e7048c8329d5006107de9853ae75f38b9ae92b73
CesarArias11 <a01411995@itesm.mx>
2019-06-06 00:38:16 -0500

    initial

---
+++ main.py
@@ -0,0 +1,27 @@
+# This is my first homework for the python course, which consits on declaring variables and then printing them
+
+# Here I declare my variables
+Name = "Careful"
+Artist = "Paramore"
+Album = "Riot!"
+YearReleased = 2007
+MonthReleased = "June"
+DayReleased = 4
+Genre = "Pop"
+Lenght = "3:31"
+DurationInSeconds = 211
+Songwriter = "Hayley Williams and Josh Farro"
+Producer = "David Bendeth"
+
+# Now, this part prints each one of those variables
+print(Name)
+print(Artist)
+print(Album)
+print(YearReleased)
+print(MonthReleased)
+print(DayReleased)
+print(Genre)
+print(Lenght)
+print(DurationInSeconds)
+print(Songwriter)
+print(Producer)
